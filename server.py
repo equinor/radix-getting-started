b4ba36f24f246efa5002bcc3e90db9a22aeac529
Joakim Hagen <johagen@equinor.com>
2020-06-29 23:38:15 +0200

    display user data

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return "Hello, World!"
+    user_id = request.headers.get('X-Forwarded-User')
+    preferred_username = request.headers.get('X-Forwarded-Preferred-Username')
+    email = request.headers.get('X-Forwarded-Email')
+    return f"User ID: {user_id}<br />Preferred username: {preferred_username}<br />E-Mail: {email}"
 
 try:
     app.run(host='0.0.0.0')
